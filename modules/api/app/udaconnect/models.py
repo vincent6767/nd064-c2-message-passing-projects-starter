791671c669998d5659b5f228dd85aa5efd32008c
Vincent <vincentbiasa6767@gmail.com>
2021-10-09 22:17:55 +0700

    Add missing location attributes for Udaconnect API service

--- modules/api/app/udaconnect/models.py
+++ modules/api/app/udaconnect/models.py
@@ -30,6 +30,33 @@ class Location(db.Model):
     creation_time = Column(DateTime, nullable=False, default=datetime.utcnow)
     _wkt_shape: str = None
 
+    @property
+    def wkt_shape(self) -> str:
+        # Persist binary form into readable text
+        if not self._wkt_shape:
+            point: Point = to_shape(self.coordinate)
+            # normalize WKT returned by to_wkt() from shapely and ST_AsText() from DB
+            self._wkt_shape = point.to_wkt().replace("POINT ", "ST_POINT")
+        return self._wkt_shape
+
+    @wkt_shape.setter
+    def wkt_shape(self, v: str) -> None:
+        self._wkt_shape = v
+
+    def set_wkt_with_coords(self, lat: str, long: str) -> str:
+        self._wkt_shape = f"ST_POINT({lat} {long})"
+        return self._wkt_shape
+
+    @hybrid_property
+    def longitude(self) -> str:
+        coord_text = self.wkt_shape
+        return coord_text[coord_text.find(" ") + 1 : coord_text.find(")")]
+
+    @hybrid_property
+    def latitude(self) -> str:
+        coord_text = self.wkt_shape
+        return coord_text[coord_text.find("(") + 1 : coord_text.find(" ")]
+
 @dataclass
 class Connection:
     location: Location
