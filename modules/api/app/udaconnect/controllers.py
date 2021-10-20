a1cae6a55af05914076e798fb258c52a62eefa16
Vincent <vincentbiasa6767@gmail.com>
2021-10-20 20:44:39 +0700

    Fix diagram and remove unnecessary todo comments

--- modules/api/app/udaconnect/controllers.py
+++ modules/api/app/udaconnect/controllers.py
@@ -15,9 +15,6 @@ DATE_FORMAT = "%Y-%m-%d"
 
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
 
-
-# TODO: This needs better exception handling
-
 @api.route("/persons")
 class PersonsResource(Resource):
     @accepts(schema=PersonSchema)
