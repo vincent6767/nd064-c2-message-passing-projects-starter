5532dfd9b227228a04b207a03d2bb988737a6698
Vincent <vincentbiasa6767@gmail.com>
2021-10-20 11:01:01 +0700

    Add keyboard interrupt and add mock value for coordinate

--- modules/locationconsumer/location_consumer.py
+++ modules/locationconsumer/location_consumer.py
@@ -1,6 +1,4 @@
-import json
-import psycopg2
-import os
+import json, psycopg2, os, sys
 from kafka import KafkaConsumer
 
 topic = "people_location"
@@ -22,30 +20,36 @@ try:
         database=db_name
     )
     cursor = connection.cursor()
-except (Exception, psycopg2.Error) as error:
+
+    for message in consumer:
+        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                            message.offset, message.key,
+                                            message.value))
+        payload = json.loads(message.value.decode('utf-8'))
+        print("Message payload: {}, Person id: {}".format(payload, payload['person_id'])) # to be removed later.
+
+        try: 
+            coordinate = '010100000000ADF9F197925EC0FDA19927D7C64240' # TODO: calculate this based on the longitude and latitude
+
+            postgres_insert_query = "INSERT INTO location (id, person_id, coordinate, creation_time) VALUES (%s, %s, %s, %s)"
+            record_to_insert = (payload['id'], payload['person_id'], coordinate , payload['creation_time'])
+
+            cursor.execute(postgres_insert_query, record_to_insert)
+
+            connection.commit()
+
+            print(cursor.count, " record inserted successfully into location table")
+        except (Exception, psycopg2.Error) as error:
+            print("Failed to insert record into location table", error)
+            print("Continue listening . . .")
+except KeyboardInterrupt:
+    print("Detected signal exit. Exiting application . . .")
+except (psycopg2.Error) as error:
     print("Failed to connect to the PostgreSQL", error)
     print("Application exit")
-    exit
-
-for message in consumer:
-    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
-    payload = json.loads(message.value.decode('utf-8'))
-    print("Message payload: {}, Person id: {}".format(payload, payload['person_id'])) # to be removed later.
-
-    try: 
-        coordinate = None # TODO: calculate this based on the longitude and latitude
-
-        postgres_insert_query = "INSERT INTO location (id, person_id, coordinate, creation_time) VALUES (%s, %s, %s, %s)"
-        record_to_insert = (payload['id'], payload['person_id'], coordinate , payload['creation_time'])
-
-        cursor.execute(postgres_insert_query, record_to_insert)
-
-        connection.commit()
-
-        print(cursor.count, " record inserted successfully into location table")
-    except (Exception, psycopg2.Error) as error:
-        print("Failed to insert record into location table", error)
-        print("Continue listening . . .")
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgresSQL connection closed")
 
