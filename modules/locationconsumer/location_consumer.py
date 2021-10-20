569c28d5307d336f99432aad7f3bd3d3b7472c37
Vincent <vincentbiasa6767@gmail.com>
2021-10-20 13:08:39 +0700

    Convert string into float for latitude and longitude

--- modules/locationconsumer/location_consumer.py
+++ modules/locationconsumer/location_consumer.py
@@ -33,7 +33,8 @@ try:
         print("Message payload: {}, Person id: {}".format(payload, payload['person_id'])) # to be removed later.
 
         try: 
-            coordinate = ST_Point(payload['latitude'], payload['longitude'])
+            payload['latitude'] = float(payload['latitude'])
+            payload['longitude'] = float(payload['longitude'])
 
             postgres_insert_query = "INSERT INTO location (id, person_id, coordinate, creation_time) VALUES (%s, %s, ST_GeomFromText('POINT(%s %s)'), %s)"
             record_to_insert = (payload['id'], payload['person_id'], payload['latitude'], payload['longitude'], payload['creation_time'])
